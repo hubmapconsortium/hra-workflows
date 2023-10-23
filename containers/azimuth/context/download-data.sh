2121eb301a08bd191031adbed25fa83847b083b3
Daniel Bolin <axbolin@iu.edu>
2023-10-23 16:33:35 -0400

    Fix bug in azimuth model downloading

--- containers/azimuth/context/download-data.sh
+++ containers/azimuth/context/download-data.sh
@@ -3,6 +3,7 @@ set -e
 
 OUTPUT_DIR=${1:-"./azimuth"}
 MAPPING_FILE=${2:-"/organ-mapping.json"}
+export R_LIBS="$OUTPUT_DIR"
 
 mkdir -p "$OUTPUT_DIR"
 Rscript /download_reference_data.R "$MAPPING_FILE" "$OUTPUT_DIR"
