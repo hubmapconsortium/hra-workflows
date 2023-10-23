a8389b2e12b62ac016cb9dcff78f45ac4247f7f9
Vicky Daiya <vickydaiya10.vd@gmail.com>
2023-10-23 12:36:07 -0400

    modified gene expr code

--- containers/gene-expression/get_gene_expression.py
+++ containers/gene-expression/get_gene_expression.py
@@ -1,39 +1,47 @@
 # TODO Dockerize!!!!!
 
-from calendar import c
+# from calendar import c
 import anndata
 import numpy as np
 import scanpy as sc
 import pandas as pd
-
-adata = anndata.read_h5ad('result.h5ad')
-adata.raw = adata
+from algorithm import add_common_arguments
+from pathlib import Path
 
 #need a filter to reomve CTs with one cell as sc.tl.rank_genes_groups gives an error
 MIN_CELLS_PER_CT = 2
-CELL_TYPE_COLUMN = "predicted.ann_finest_level"
+CELL_TYPE_COLUMN = "predicted.ann_finest_level" #from annotations.csv
+COUNT_COLUMN = "count" #from annotations.csv
 
-ct_counts = adata.obs[CELL_TYPE_COLUMN].value_counts()
-valid_cts = ct_counts.index[ct_counts >= MIN_CELLS_PER_CT]
-mask = np.isin(adata.obs[CELL_TYPE_COLUMN], valid_cts)
+def get_marker_genes(output_matrix: Path, output_annotations: Path):
+    adata = anndata.read_h5ad(output_matrix)
+    ct_counts = pd.read_csv(output_annotations)
+    # adata.raw = adata
 
-filtered_adata = adata[mask, :]
+    valid_cts = ct_counts.loc[ct_counts[COUNT_COLUMN] >= MIN_CELLS_PER_CT,CELL_TYPE_COLUMN]
+    mask = np.isin(adata.obs[CELL_TYPE_COLUMN], valid_cts)
+    filtered_adata = adata[mask, :]
 
-sc.tl.rank_genes_groups(filtered_adata, groupby=CELL_TYPE_COLUMN, n_genes=10)
+    sc.tl.rank_genes_groups(filtered_adata, groupby=CELL_TYPE_COLUMN, n_genes=10)
 
-ct_gene_expr = pd.DataFrame(filtered_adata.uns['rank_genes_groups']['names']).transpose()
+    ct_marker_genes = pd.DataFrame(filtered_adata.uns['rank_genes_groups']['names']).transpose()
+    ct_marker_genes['marker_genes'] = ct_marker_genes.apply(lambda row: row.tolist(), axis=1)
+    ct_marker_genes = ct_marker_genes['marker_genes'].rename_axis(CELL_TYPE_COLUMN).reset_index()
 
-ct_gene_expr['gene_expr'] = ct_gene_expr.apply(lambda row: row.tolist(), axis=1)
+    def get_expr_value(cell_type,marker_genes):
+        output = []
+        cell_indices = [filtered_adata.obs.index.get_loc(cell_index) for cell_index in filtered_adata.obs[filtered_adata.obs[CELL_TYPE_COLUMN] == ct].index]
+        for gene in marker_genes:
+            output.append({'gene_label':gene,'mean_gene_expr_value':filtered_adata.X[cell_indices,filtered_adata.var.index.get_loc(gene)].mean()})
+        return output
 
-ct_gene_expr = ct_gene_expr['gene_expr'].rename_axis('ct').reset_index()
+    ct_marker_genes['gene_expr'] = ct_marker_genes.apply(lambda row: get_expr_value(row['ct'], row['marker_genes']), axis=1)
 
-def get_gene_expr(ct,genes):
-    output = []
-    cell_indices = [filtered_adata.obs.index.get_loc(cell_index) for cell_index in filtered_adata.obs[filtered_adata.obs[CELL_TYPE_COLUMN] == ct].index]
-    for gene in genes:
-        output.append({'gene_label':gene,'mean_gene_expr_value':filtered_adata.X[cell_indices,filtered_adata.var.index.get_loc(gene)].mean()})
-    return output
+    return ct_counts.merge(ct_marker_genes[[CELL_TYPE_COLUMN,'gene_expr']],how='left')
 
-ct_gene_expr['gene_expr'] = ct_gene_expr.apply(lambda row: get_gene_expr(row['ct'], row['gene_expr']), axis=1)
 
-ct_gene_expr.to_csv('gene_expr.csv')
+if __name__ == "__main__":
+    parser = add_common_arguments()
+    args = parser.parse_args()
+    result = get_marker_genes(**args.__dict__)
+    result.save()
