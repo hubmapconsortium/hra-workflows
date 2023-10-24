6bab6873955c0499dcf9492c70221358037c4a4d
Vicky Daiya <vickydaiya10.vd@gmail.com>
2023-10-24 13:28:26 -0400

    Fix minor bug in build-containers

--- scripts/build-containers.sh
+++ scripts/build-containers.sh
@@ -6,6 +6,6 @@ ALL_CONTAINERS=$(basename -a ./containers/*)
 CONTAINERS=${@:-$ALL_CONTAINERS}
 
 for NAME in $CONTAINERS; do
-  cp -r ./src/ ./containers/$NAME/context/
+  cp -r ./src ./containers/$NAME/context/
   docker build -t ghcr.io/hubmapconsortium/hra-workflows/$NAME:main ./containers/$NAME/
 done
