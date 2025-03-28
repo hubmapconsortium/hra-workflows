f44151f2eae461c916916cc3e323ad909c3900c6
Bruce W. Herr II <bherr@iu.edu>
2025-03-28 15:21:29 -0400

    Use a specific commit for the ontology used by popv

--- containers/popv/context/download-ontology.sh
+++ containers/popv/context/download-ontology.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ONTOLOGY_DIR=./ontology
-ONTOLOGY_DIR_URL=https://raw.githubusercontent.com/YosefLab/PopV/main/resources/ontology
+ONTOLOGY_DIR_URL=https://raw.githubusercontent.com/YosefLab/PopV/2d29c9a290d2015ec65ef0ef9f0e6b6d2277e7bb/resources/ontology
 
 mkdir -p $ONTOLOGY_DIR
 cd $ONTOLOGY_DIR
